2c11293208b53c5504eee016574102f01e4ff6ed
kilian <kiliantennyson@live.ie>
2019-08-24 18:48:16 +0100

    new layout

---
+++ maps/Spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Spritesheet" tilewidth="64" tileheight="64" tilecount="400" columns="20">
+ <image source="Spritesheet.png" width="1280" height="1280"/>
+</tileset>
